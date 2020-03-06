f6f75eb09296757394e3f4ceee975779ead9bdb8
Steeve ASSOUS <steeve@ajc-formation.fr>
2020-03-06 10:11:15 +0100

    v2

--- prjJeanChristopheGIT/src/prjJeanChristopheGIT/Test.java
+++ prjJeanChristopheGIT/src/prjJeanChristopheGIT/Test.java
@@ -4,7 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("projet JeanChristophe git 1");
+		System.out.println("projet JeanChristophe git v1");
+		System.out.println("projet JeanChristophe git v2");
 	}
 
 }
