2942724bc6fe4208c8f0c75a6ab331c012e355dc
Steeve ASSOUS <steeve@ajc-formation.fr>
2020-03-06 10:35:34 +0100

    V4

--- prjJeanChristopheGIT/src/prjJeanChristopheGIT/Test.java
+++ prjJeanChristopheGIT/src/prjJeanChristopheGIT/Test.java
@@ -7,6 +7,9 @@ public class Test {
 		System.out.println("projet JeanChristophe git v1");
 		System.out.println("projet JeanChristophe git v2");
 		System.out.println("projet JeanChristophe git v3");
+		System.out.println("projet JeanChristophe git v4");
+		
+//      http://10.0.8.229/svn/svnklee/
 	}
 	
 }
