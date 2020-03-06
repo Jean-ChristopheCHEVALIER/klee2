3ebd6674c79f8eb7bf41a7f4ea8fa0d297d7fab2
Steeve ASSOUS <steeve@ajc-formation.fr>
2020-03-06 10:09:02 +0100

    v1

---
+++ prjJeanChristopheGIT/src/prjJeanChristopheGIT/Test.java
@@ -0,0 +1,10 @@
+package prjJeanChristopheGIT;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("projet JeanChristophe git 1");
+	}
+
+}
